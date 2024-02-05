abc6965b2e7db2b92f9d695004aee38e4b2d5ab7
Alvin Nguyen <alvin.nguyen@veeva.com>
2024-02-04 17:07:24 -0800

    Initial Commit

---
+++ HackerNewsApp/HackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAppApp.swift
+//  HackerNewsApp
+//
+//  Created by Alvin Nguyen on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
