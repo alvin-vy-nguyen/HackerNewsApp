428bad61978dfc716482252a858fec0aa390c747
Alvin Nguyen <alvin.nguyen@veeva.com>
2024-02-07 21:03:59 -0800

    Edit NavBar background color as well as the ListView background color. Added some comments as well

--- HackerNewsApp/Views/ContentView.swift
+++ HackerNewsApp/Views/ContentView.swift
@@ -11,9 +11,20 @@ struct ContentView: View {
     
     @ObservedObject var networkManager = NetworkManager()
     
+    init() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = UIColor.cyan
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    
     var body: some View {
+        // NavigationStack allows us to move between views
+        // Used in conjuction with NavigationLink
+        
         NavigationStack {
             List(networkManager.posts) { post in
+                // NavigationLink creates an arrow button on the right which can create a view on top of the root view
                 NavigationLink(destination: DetailView(url: post.url)) {
                     HStack {
                         Text(String(post.points))
@@ -21,6 +32,8 @@ struct ContentView: View {
                     }
                 }
             }
+            .background(Color.teal)
+            .scrollContentBackground(.hidden)
             .navigationTitle("Hacker News")
         }
         // This method is like our ViewDidLoad
